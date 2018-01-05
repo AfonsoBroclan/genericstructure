900cd39b6be113b8000dc0099b7b6e9656b1357f
Afonso Rosa <afonsorosa@pedros-imac.local>
2018-01-05 11:39:52 +0000

    first commit

---
+++ Generic Structure/Generic Structure/ViewControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Generic Structure
+//
+//  Created by Afonso Rosa on 05/01/18.
+//  Copyright © 2018 Afonso Rosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
