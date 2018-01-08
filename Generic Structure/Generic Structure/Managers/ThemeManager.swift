6403b89f45efe11d8645c461d8ad2a2beb723d57
Afonso Rosa <afonsorosa@Pedros-iMac.local>
2018-01-08 11:45:59 +0000

    added theme manager

---
+++ Generic Structure/Generic Structure/Managers/ThemeManager.swift
@@ -0,0 +1,16 @@
+//
+//  ThemeManager.swift
+//  Generic Structure
+//
+//  Created by Afonso Rosa on 08/01/18.
+//  Copyright © 2018 Afonso Rosa. All rights reserved.
+//
+
+import Foundation
+
+class ThemeManager {
+    
+    static let sharedInstance = ThemeManager()
+    private init() {} //This prevents others from using the default '()' initializer for this class.
+    
+}
